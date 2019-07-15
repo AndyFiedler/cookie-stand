a6702d8a272a1efa685a7c13567b539b45627121
Andy Fiedler <andyfidd@gmail.com>
2019-07-15 14:44:58 -0500

    basic objects set up

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//1st and Pike
+var pike = {
+    minCustomers: 23,
+    maxCustomers: 65,
+    cookieAverage: 6.3,
+
+}
+
+//SeaTac Aiport
+var seaTac = {
+    minCustomers: 3,
+    maxCustomers: 24,
+    cookieAverage: 1.2,
+
+}
+
+//Seattle Center
+var seattleCen = {
+    minCustomers: 11,
+    maxCustomers: 38,
+    cookieAverage: 3.7,
+
+}
+
+//Capitol Hill
+var capitolHill = {
+    minCustomers: 20,
+    maxCustomers: 38,
+    cookieAverage: 2.3,
+
+}
+
+//Alki
+var alki = {
+    minCustomers: 2,
+    maxCustomers: 16,
+    cookieAverage: 4.6,
+    
+}
